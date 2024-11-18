09262a34b6eae43fd32ed373117232745dbbce0e
Vinod Nair <vnair-consultant@scholastic.com>
2024-11-18 17:19:06 +0530

    Basic login with hashcheck done

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, redirect, flash, jsonify
-import bcrypt
+import hashlib
 import json
 from google.oauth2 import service_account
 from googleapiclient.discovery import build
@@ -11,20 +11,20 @@ from waitress import serve
 
 app = Flask(__name__)
 app.secret_key = 'ThisIsMyReallyLongSecret#ey'
-DATA_FILE = "data/users.json"
+
 # Initialize Google Sheets API
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
-SERVICE_ACCOUNT_FILE = '/json/myutils-437714-bd0d0a3e77bd.json'  # Update this path
+SERVICE_ACCOUNT_FILE = 'json/myutils-437714-bd0d0a3e77bd.json'  # Update this path
 creds = service_account.Credentials.from_service_account_file(
     SERVICE_ACCOUNT_FILE, scopes=SCOPES)
 
 USERS_SHEET_ID = '1_YhYay1XVZ9rvVUf85vZR-ZpSbwdkDoP_b-CWCfklso'
 USERS_RANGE_NAME = 'USERS!A2:E' 
 
-# Ensure the data file exists
-if not os.path.exists(DATA_FILE):
-    with open(DATA_FILE, "w") as file:
-        json.dump([], file)
+
+def encrypt_with_hash(input_string):
+    """Encrypts a string using SHA-256 hashing (deterministic)."""
+    return hashlib.sha256(input_string.encode()).hexdigest()
 
 def get_user(username):
     #"""Read all users from the JSON file."""
@@ -77,7 +77,7 @@ def register():
             flash("User already exists!", "warning")
             return redirect('/register')
 
-        hashed_password = str(bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()))
+        hashed_password = encrypt_with_hash(password)
         
         write_user(username,hashed_password,"","")
 
@@ -86,6 +86,10 @@ def register():
 
     return render_template('register.html')
 
+@app.route('/', methods=['GET', 'POST'])
+def redirect():
+    return render_template('login.html', message="")
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
@@ -93,18 +97,18 @@ def login():
         password = request.form['password']
 
         if not username or not password:
-            flash("All fields are required!", "danger")
-            return redirect('/login')
-
+            return render_template('login.html', message="All fields are required!")
+            
         iuser = get_user(username)
-        hashed_password = str(bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()))
+        hashed_password = encrypt_with_hash(password)
 
         if iuser and iuser[0][1] == hashed_password:
-            flash("Login successful!", "success")
-            return redirect('/register')
+            return render_template('landing.html', message="Login successful!")
+        else:
+            return render_template('login.html', message="Invalid Username or Password. Retry !!!")
+            
 
-        flash("Invalid username or password!", "danger")
-        return redirect('/login')
+ 
 
     return render_template('login.html')
 
